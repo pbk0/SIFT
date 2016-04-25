b0b64cd575256f32dd093f88994449b7dcb326e8
praveenneuron <praveenneuron@gmail.com>
2016-04-26 01:32:22 +0200

    haha my first cmake script :)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 
 #define _VIGRA 0
+#define _OPENCV 1
 
 #if _VIGRA
 #include <vigra/impex.hxx>
@@ -10,7 +11,10 @@
 #include <vigra/gaborfilter.hxx>
 #include <vigra/edgedetection.hxx>
 #include <vigra/hdf5impex.hxx>
-#else
+#endif
+
+#if _OPENCV
+#include <opencv2/opencv.hpp>
 #include <opencv2/xfeatures2d/nonfree.hpp>
 #endif
 
@@ -20,8 +24,14 @@ int main() {
 
 #if _VIGRA
     cout << "Hello, World! ... Using VIGRA" << endl;
-#else
-    cout << "Hello, World! ... Using OpenCV" << endl;
 #endif
+
+#if _OPENCV
+    cout << "Hello, World! ... Using OpenCV" << endl;
+    string img_name = "img1.png";
+    cv::Mat img = cv::imread(img_name, CV_LOAD_IMAGE_COLOR);
+#endif
+
+
     return 0;
 }
